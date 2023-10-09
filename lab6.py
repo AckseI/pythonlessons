d546e74ff74cae820088e844ece9d763569d8e6a
Acksel <egortrifo@gmail.com>
2023-10-10 00:08:20 +0300

    lab6

---
+++ lab6.py
@@ -0,0 +1,39 @@
+import sqlite3
+import json
+
+conn = sqlite3.connect('files/librarydb-sqlite.db')
+cursor = conn.cursor()
+
+# cursor.execute('''
+# CREATE TABLE IF NOT EXISTS Users (
+# id INTEGER PRIMARY KEY,
+# username TEXT NOT NULL,
+# email TEXT NOT NULL,
+# age INTEGER
+# )
+# ''')
+
+# cursor.execute("""
+#     INSERT INTO Users (username, email, age)
+#     VALUES ('Acksel', 'Acksel.com', 20),
+#     ('Chiya', 'Chiya.com', 18),
+#     ('Smork', 'Smork.com', 21)
+#     """)
+
+
+
+# with open('files/lab6.json', 'w') as f:
+
+cursor.execute("""
+    SELECT *
+    FROM Users
+""")
+
+results = cursor.fetchall()
+
+with open('files/new_file.json', 'w') as f:
+    json.dump(results, f, indent=2)
+    print("New json file is created from data.json file")
+
+#conn.commit()
+conn.close()
